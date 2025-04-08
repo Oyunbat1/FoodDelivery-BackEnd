341f2248c9d8d25e70054f8cfceb9aea1b8ed81c
Oyunbat <oyunbat9958@gmail.com>
2025-04-08 12:21:14 +0800

    backend done

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import express, { Request, Response } from "express";
 import { connectDb } from "./utils/connection";
 import foodRouter from "./routes/food/Food";
 import { categoryRouter } from "./routes/category/createCategory";
-
+import cors from "cors";
 const port = 8000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/foods", foodRouter);
+
 app.use("/api/v1/categories", categoryRouter);
 
 app.listen(port, async () => {
