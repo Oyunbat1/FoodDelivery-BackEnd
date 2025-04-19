99fc25e44cbc4a9b160b2cf068cf6531b76b30ac
Oyunbat <oyunbat9958@gmail.com>
2025-04-19 23:30:09 +0800

    user check in resetpassword

--- src/controllers/reset-password.ts
+++ src/controllers/reset-password.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from "express";
 import nodemailer from "nodemailer";
+import { User } from "../schema/User";
+import jwt from "jsonwebtoken";
 export const resetRequest = async (req: Request, res: Response) => {
+  const ACCESS_TOKEN_SECRET_KEY = "Oyunbat1216$";
+  const { userEmail } = req.body;
+
+  const user = await User.findOne({ email: userEmail });
+
+  if (!user) {
+    res.send({ message: "User not found" });
+    return;
+  }
+
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
@@ -10,15 +22,17 @@ export const resetRequest = async (req: Request, res: Response) => {
       pass: "roaahjaypaftwrxh ",
     },
   });
+  const token = jwt.sign({ user }, ACCESS_TOKEN_SECRET_KEY, {
+    expiresIn: "1h",
+  });
   const info = await transporter.sendMail({
     from: '"Пасспортоо сэргээгээрэй залуу" <maddison53@ethereal.email>',
     to: "oyunbat9958@gmail.com",
     subject: " Пасспорт сэргээх✔",
     text: "Пасспортоо сэргээхийн тулд та доох линкен дээр дарна уу...",
-    html: `<h1 style="color:red">Пасспорт сэргээх</h1> <p>Энд дарах <a href="http://localhost:3000/">энд</a>Пасспорт сэргээх</p>`,
+    html: `<h1 style="color:red">Пасспорт сэргээх</h1> 
+    <p>Энд дарах <a href="http://localhost:3000/reset-password?token=${token}">энд</a>Пасспорт сэргээх</p>`,
   });
   console.log(info);
   res.send({ message: "Mail sent" });
 };
-//  //ингэснээх миний майлыг ашиглах боломжтой болж байна.
-//
